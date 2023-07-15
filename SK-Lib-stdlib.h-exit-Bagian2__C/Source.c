c5ff51140b3b6c882e15184e2b96f95d34447a1c
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-07-15 07:58:54 +0700

    Initial commit

---
+++ SK-Lib-stdlib.h-exit-Bagian2__C/Source.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+/*
+    Source by GeeksForGeeks (https://www.geeksforgeeks.org)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main() {
+    printf("START");
+    exit(0);
+    printf("End of program");
+
+    _getch();
+    return 0;
+}
